1300663d0ec2ace88783ae476d5037b42acf3601
alycia hall <amhall25@gmail.com>
2019-10-29 01:09:13 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,7 +3,7 @@ class Cat
   attr_reader :name
   @@all=[]
   def initialize(name, owner)
-    @owner = Owner.new(owner).name
+    @owner = owner
     @name = name
     @mood = "nervous"
     @@all<< self
